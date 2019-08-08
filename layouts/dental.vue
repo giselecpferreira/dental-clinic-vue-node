78531186f9a178b3d42f175361638fd8aab616f1
Walmag Santos <walmag@MacBook-Pro-de-Walmag.local>
2019-08-08 07:03:14 -0300

    first commit project

---
+++ layouts/dental.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app class=˜image˜>
+        <div>
+            <v-content>
+                <v-container>
+                    <nuxt />
+                </v-container>
+            </v-content>
+        </div>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.image {
+    position:relative;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(rgba(24, 23, 26, 0.75),
+      rgba(24, 23, 26, 0.75)),
+      url(~static/login.jpeg);
+      background-repeat: no-repeat;
+      background-size: cover;
+
+}
+
+.layout {
+    height: 80vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+       oooo
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          123123
+          </v-card-title> 
+        <v-card-text>
+         456456
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  layout: 'dental',
+
+
+methods: {
+  loginEnter() {
+    if(this.$refs.formLogin.validate() ){
+      this.loadind = true;
+      setTimeout(() => this.$router.push('/') , 3000)
+    
+  }
+}
+}
+}
+</script>
